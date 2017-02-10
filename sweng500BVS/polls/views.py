ecd33a3794a14108f08aabebb667c3bf538adcf5
mxl5588 <mxl5588@psu.edu>
2017-02-09 23:03:21 -0500

    added detail view

--- sweng500BVS/polls/views.py
+++ sweng500BVS/polls/views.py
@@ -1,29 +1,42 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
-
+from django.shortcuts import render
 from .models import Question
-
-
+# Create your views here.
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
+	#template = loader.get_template('polls/index.html')
 	context = { 
 		'latest_question_list' : latest_question_list,
 	 }
+	
+	return render(request, 'polls/index.html', context)
+	#return HttpResponse(template.render(context, request))
 
 	#output = ', '.join([q.question_text for q in latest_question_list])
 	#return HttpResponse(output)
-	return HttpResponse(template.render(context, request))
 
+#rewritten detail view
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
+# detail view v2	
+#def detail(request, question_id):
+#	try:
+#		question = Question.objects.get(pk=question_id)
+#	except Question.DoesNotExist:
+#		raise Http404("Question does not exist")
+#	return render(request, 'polls/detail.html', {'question': question})
+
+# detail view v1
+#def detail(request, question_id):
+#	return HttpResponse("You're looking at question %s." % question_id)
 
 
 def results(request, question_id):
