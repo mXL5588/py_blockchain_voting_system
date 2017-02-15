fb4fe563835e5c4d22f04df68b420e6007165230
mxl5588 <mxl5588@psu.edu>
2017-02-14 23:46:15 -0500

    began adding css

--- sweng500BVS/polls/models.py
+++ sweng500BVS/polls/models.py
@@ -8,19 +8,19 @@ import datetime
 # Create your models here.
 
 
-class Question(models.Model):
-	question_text = models.CharField(max_length=200)
+class Ballot(models.Model):
+	ballot_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
 	def __str__(self):
-		return self.question_text
+		return self.ballot_text
 
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	ballot = models.ForeignKey(Ballot, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
 	
--- sweng500BVS/polls/views.py
+++ sweng500BVS/polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.urls import reverse
 from django.template import loader
 from django.shortcuts import render
-from .models import Choice, Question
+from .models import Ballot, Choice
 from django.views import generic
 
 # Create your views here.
@@ -14,54 +14,54 @@ from django.views import generic
 
 ##index manual vie
 #def index(request):
-#	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#	latest_ballot_list = ballot.objects.order_by('-pub_date')[:5]
 #	#template = loader.get_template('polls/index.html')
 #	context = { 
-#		'latest_question_list' : latest_question_list,
+#		'latest_ballot_list' : latest_ballot_list,
 #	 }
 #	
 #	return render(request, 'polls/index.html', context)
 	#return HttpResponse(template.render(context, request))
 
-	#output = ', '.join([q.question_text for q in latest_question_list])
+	#output = ', '.join([q.ballot_text for q in latest_ballot_list])
 	#return HttpResponse(output)
 
 ##rewritten detail view
-#def detail(request, question_id):
-#	question = get_object_or_404(Question, pk=question_id)
-#	return render(request, 'polls/detail.html', {'question': question})
+#def detail(request, ballot_id):
+#	ballot = get_object_or_404(ballot, pk=ballot_id)
+#	return render(request, 'polls/detail.html', {'ballot': ballot})
 
 ## detail view v2	
-#def detail(request, question_id):
+#def detail(request, ballot_id):
 #	try:
-#		question = Question.objects.get(pk=question_id)
-#	except Question.DoesNotExist:
-#		raise Http404("Question does not exist")
-#	return render(request, 'polls/detail.html', {'question': question})
+#		ballot = ballot.objects.get(pk=ballot_id)
+#	except ballot.DoesNotExist:
+#		raise Http404("ballot does not exist")
+#	return render(request, 'polls/detail.html', {'ballot': ballot})
 
 # detail view v1
-#def detail(request, question_id):
-#	return HttpResponse("You're looking at question %s." % question_id)
+#def detail(request, ballot_id):
+#	return HttpResponse("You're looking at ballot %s." % ballot_id)
 
 ##manual view 
-#def results(request, question_id):
-#	#response = "You're looking at the response of question %s." 
-#	#return HttpResponse(response % question_id)
-#	question = get_object_or_404(Question, pk=question_id)
-#	return render(request , 'polls/results.html', {'question':question})
+#def results(request, ballot_id):
+#	#response = "You're looking at the response of ballot %s." 
+#	#return HttpResponse(response % ballot_id)
+#	ballot = get_object_or_404(ballot, pk=ballot_id)
+#	return render(request , 'polls/results.html', {'ballot':ballot})
 
 
-def vote(request, question_id):
-	#return HttpResponse("You're voting on question %s" % question_id)
-	question = get_object_or_404(Question, pk=question_id)
+def vote(request, ballot_id):
+	#return HttpResponse("You're voting on ballot %s" % ballot_id)
+	ballot = get_object_or_404(Ballot, pk=ballot_id)
 
 	try:
-			selected_choice = question.choice_set.get(pk=request.POST['choice'])
+			selected_choice = ballot.choice_set.get(pk=request.POST['choice'])
 
 	except (KeyError, Choice.DoesNotExist):
-		# Redisplay the question voting form
+		# Redisplay the ballot voting form
 		return render(request, 'polls/detail.html', 
-			{ 'question': question, 'error_message': "You didn't select a choice.",})
+			{ 'ballot': ballot, 'error_message': "You didn't select a choice.",})
 	else:
 		selected_choice.votes += 1
 		selected_choice.save()
@@ -70,24 +70,24 @@ def vote(request, question_id):
 		# with POST data. This prevents data from being posted twice if a
 		# user hits the back button.
 
-		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+		return HttpResponseRedirect(reverse('polls:results', args=(ballot.id,)))
 
 
 class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
-	context_object_name = 'latest_question_list'
+	context_object_name = 'latest_ballot_list'
 
 	def get_queryset(self):
-		"""Return the last five published question"""
-		return Question.objects.order_by('-pub_date')[:5]
+		"""Return the last five published ballot"""
+		return Ballot.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
-	model = Question
+	model = Ballot
 	template_name = 'polls/detail.html'
 
 
 class ResultsView(generic.DetailView):
-	model = Question
+	model = Ballot
 	template_name = 'polls/results.html'
 	
