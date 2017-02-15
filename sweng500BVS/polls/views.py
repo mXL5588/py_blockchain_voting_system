f64562fecedd862796c8cb672832a4f80edf6b1e
mxl5588 <mxl5588@psu.edu>
2017-02-14 21:30:23 -0500

    fixed some typos

--- sweng500BVS/polls/views.py
+++ sweng500BVS/polls/views.py
@@ -75,11 +75,11 @@ def vote(request, question_id):
 
 class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
-	context_object_name = 'latest question listed'
+	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
 		"""Return the last five published question"""
-		return Quesiton.objects.order_by('-pub_date')[:5]
+		return Question.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
